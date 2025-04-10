7890b3da91d324ab4aba6b50d503d1daf9a63500
Uragami Taichi <backpaper0@gmail.com>
2025-04-10 23:06:16 +0900

    Update search_document API to prevent Unicode escape

--- nabchan_mcp_server/main.py
+++ nabchan_mcp_server/main.py
@@ -2,6 +2,7 @@
 MCPサーバー本体。
 """
 
+import json
 from typing import TypedDict
 from mcp.server.fastmcp import FastMCP
 from whoosh.index import open_dir
@@ -31,25 +32,28 @@ mcp = FastMCP("nabchan", host=args.host, port=args.port)
 
 @mcp.tool(description="URLが示すNablarchのドキュメントを返します。")
 def read_document(url: str) -> str:
-    query = Term("url", url)
     with index.searcher() as searcher:
+        query = Term("url", url)
         results = searcher.search(query)
         return results[0]["markdown"] if results else ""
 
 
 @mcp.tool(description="Nablarchのドキュメントを検索します。")
-def search_document(search_query: str, result_limit: int) -> list[SearchResult]:
+def search_document(search_query: str, result_limit: int) -> str:
     with index.searcher() as searcher:
         query = QueryParser("content", index.schema).parse(search_query)
-        results = searcher.search(query, limit=result_limit)
-        return [
+        results = [
             SearchResult(
                 url=result["url"],
                 title=result["title"],
                 description=result["description"],
             )
-            for result in results
+            for result in searcher.search(query, limit=result_limit)
         ]
+        # mcp/server/fastmcp/server.pyの_convert_to_content関数で素朴にjson.dumps()
+        # されているためUnicodeエスケープされないためにはstr、あるいはTextContent型で返す必要がある。
+        # ここではstrで返す。
+        return json.dumps(results, ensure_ascii=False)
 
 
 if __name__ == "__main__":
